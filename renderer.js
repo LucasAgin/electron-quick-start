4d7114f93ebef51fc57e8051754bc5b972082c7b
Lucas Agin <lucas@forefrontweb.com>
2017-07-25 09:28:25 -0400

    Firebase setup

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var database = firebase.database();
+// var quoteRef = FIRDatabase.database().reference(withPath: "Quotes")
+var quoteRef = firebase.database().ref('Quotes');
+quoteRef.on('value', function(snapshot) {
+  snapshot.forEach(function(child){
+        var key = child.key;
+        var value = child.val();
+        console.log(key);
+        console.log(value);
+    });
+});
